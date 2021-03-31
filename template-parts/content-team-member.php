97af5beaf6394f6148ba84431284444cf05de9f8
Nori <loveandfight2186@yahoo.co.jp>
2021-03-31 21:03:20 +0900

    add border radius for team prof image

--- template-parts/content-team-member.php
+++ template-parts/content-team-member.php
@@ -1,10 +1,7 @@
 <?php ?>
 <article <?php post_class(); ?>>
-  <div class="post-thumbnail">
-    <?php the_post_thumbnail(); ?>
-  </div>
   <div class="upper-position">
-    <div class="member-name">
+    <div class="team__member-name">
       <?php the_field('name'); ?>
     </div>
   </div>
@@ -12,12 +9,12 @@
   <div class="right-top"></div>
   <div class="left-bottom"></div>
   <div class="right-bottom"></div>
-  <div class="member-profile-image">
+  <div class="team__member-profile-image">
     <?php 
       $image = get_field('profile_image');
-      // $size = 'full'; // (thumbnail, medium, large, full or custom size)
+      $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
       if( $image ) {
-          echo wp_get_attachment_image($image);
+          echo wp_get_attachment_image($image, $size);
       }
     ?>
 
