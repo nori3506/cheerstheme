617eacf66af7132190fa4cd9ad478fff36446727
Nori <loveandfight2186@yahoo.co.jp>
2021-04-11 00:23:20 +0900

    add team__prof-image class in content-team-member-intro.php

--- template-parts/content-team-member-intro.php
+++ template-parts/content-team-member-intro.php
@@ -1,4 +1,9 @@
 <div class="team__intro-area">
+
+  <div class="team__prof-image">
+    <img src="<?php the_field('profile_image'); ?>" alt="profile image">
+  </div>
+
   <div class="team__full-name">
     <h2><?php the_field('name'); ?></h2>
   </div>
